e18fdabfddd0bc3882ed9c7644eabe1a41b950d0
Jesus Adrian Garcia Perez <jesusgp_23_98@outlook.es>
2023-01-03 20:30:11 -0600

    ultimo commit mac

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoggingService } from '../../LoggingService.service';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../personas.service';
@@ -9,7 +9,7 @@ import { PersonasService } from '../../personas.service';
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css'],
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
 
   //@Output() personaCreada = new EventEmitter<Persona>();
 
@@ -18,9 +18,11 @@ export class FormularioComponent {
 
   nombre: string; 
   apellido: string; 
+  index: number;
+  modoEdicion: number;
 
   constructor(private loggingService: LoggingService,
-    private personaService: PersonasService, private router: Router){
+    private personaService: PersonasService, private router: Router, private route: ActivatedRoute){
       this.personaService.saludar.subscribe(
         (indice: number) => {
           indice++;
@@ -29,17 +31,42 @@ export class FormularioComponent {
       );
     }
 
+  ngOnInit() {
+    this.index = this.route.snapshot.params['id'];
+    this.modoEdicion = +this.route.snapshot.queryParams['modoEdicion'];
+
+    if(this.modoEdicion !== null && this.modoEdicion === 1){
+      let persona: Persona = this.personaService.encontrarPersona(this.index);
+      this.nombre = persona.nombre;
+      this.apellido = persona.apellido;
+    }
+  }
+
   onGuardarPersona() {
     let persona = new Persona(
       this.nombre, 
       this.apellido);
+
+    if(this.modoEdicion === 1){
+      this.personaService.modificarPersona(this.index, persona);
+    } else {
+      this.personaService.agregarPersona(persona);
+    }
+
+    this.router.navigate(['personas']);
     //this.loggingService.enviaMensajeaConsola(`Enviamos persona: ${persona.nombre} ${persona.apellido}`);
     /**
      * con emit mandamos el objeto persona al objeto padre
      */
     //this.personaCreada.emit(persona);
-    this.personaService.agregarPersona(persona);
-    this.router.navigate(['personas']);
+    
+  }
+
+  eliminarPersona() {
+    if(this.index !== null){
+      this.personaService.eliminarPersona(this.index);
+      this.router.navigate(['personas']);
+    }
   }
 
 }
